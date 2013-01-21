a00a1f271b5dfa97bba061d2ef665b1d7b15d13d
Víctor Puertas <vpgugr@gmail.com>
2013-01-21 20:22:12 +0100

    set the language domain

--- searchform.php
+++ searchform.php
@@ -1,4 +1,5 @@
 <form class="form-search" role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
-  <input type="search" id="s" name="s" class="input-medium search-query" placeholder="<?php _e('Search') ?>">
-  <button type="submit" class="btn"><?php _e('Search') ?></button>
+  <input type="search" id="s" name="s" class="input-medium search-query" placeholder="<?php _e('Search', 'boopress') ?>">
+  <button type="submit" class="btn"><?php _e('Search', 'boopress') ?></button>
 </form>
+
